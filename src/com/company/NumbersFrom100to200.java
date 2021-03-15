4fedcd7f92b101c7034522cce0d4f8f233583d55
Stanislav <Stanislav.Ivanov@yahoo.com>
2021-03-15 10:44:53 +0200

    Greater, less or between

---
+++ src/com/company/NumbersFrom100to200.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+public class NumbersFrom100to200 {
+    public static void main (String[] arg) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num = Integer.parseInt(scanner.nextLine());
+
+        if (num < 100) {
+            System.out.println("Less than 100");
+        } else if (num > 200) {
+            System.out.println("Greater than 200");
+        } else {
+            System.out.println("Between 100 and 200");
+        }
+    }
+}

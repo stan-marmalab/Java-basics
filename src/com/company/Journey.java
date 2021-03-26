6d184147d81072addcf64a9d5651d68bd8c189ac
Stanislav <Stanislav.Ivanov@yahoo.com>
2021-03-26 15:03:12 +0200

    Fixing typo in class name

--- src/com/company/Journey.java
+++ src/com/company/Journey.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.util.Scanner;
-public class Jorney {
+public class Journey {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 

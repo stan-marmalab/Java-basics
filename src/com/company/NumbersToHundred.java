a335b78b6c4ed2aa8ab2360e1bfc0dd2bf10eebc
Stanislav <Stanislav.Ivanov@yahoo.com>
2021-03-29 14:10:10 +0300

    Numbers from 1 to 100

---
+++ src/com/company/NumbersToHundred.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+public class NumbersToHundred {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for ( int i = 1; i <= 100; i++ ) {
+            System.out.println(i);
+        }
+    }
+}

48ca73d63800a8fd280e4ea370c2cf45286fe208
Stanislav <Stanislav.Ivanov@yahoo.com>
2021-03-29 15:41:59 +0300

    Removed the if/else block since it take a step of 2 (evens)

--- src/com/company/EvenPowersOfTwo.java
+++ src/com/company/EvenPowersOfTwo.java
@@ -11,6 +11,9 @@ public class EvenPowersOfTwo {
 
         for (int i = 0; i <= n; i += 2) {
             System.out.println(number);
+
+            // since it takes step +2, we need to multiply by 4
+            // because 2 to the power of 0 is 1, 2 to the power of 2 is 4, 2 to the power of 4 is 16 ... the evens
             number *= 4;
         }
     }

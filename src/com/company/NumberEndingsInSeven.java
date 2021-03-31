db716b2c5a9a8753e0b991586d4d00bbc8c51d0d
Stanislav <Stanislav.Ivanov@yahoo.com>
2021-03-31 10:02:54 +0300

    Print number ending on 7 till 1000

---
+++ src/com/company/NumberEndingsInSeven.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+public class NumberEndingsInSeven {
+    public static void main(String[] args) {
+
+        for (int i = 7; i <= 997; i+=10) {
+            System.out.println(i);
+        }
+    }
+}
